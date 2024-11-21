dc501a307b03dea21b395691328f84dc7ef678ac
Gabe Cook <gabe565@gmail.com>
2024-11-21 01:13:35 -0600

    chore: Rearrange code

--- main.go
+++ main.go
@@ -14,31 +14,29 @@ func main() {
 	style := lipgloss.NewStyle().Padding(0, 1).Foreground(lipgloss.Color("#fff"))
 
 	for i := range 256 {
-		switch {
-		case i == 0:
-		case i <= 16:
-			switch {
-			case i == 16:
-				fmt.Print("\n\n")
-			case i == 8:
-				fmt.Println()
-			}
-		default:
-			switch {
-			case (i-16)%36 == 0:
-				fmt.Print("\n\n")
-			case (i-16)%6 == 0:
-				fmt.Println()
-			}
-		}
-
 		str := fmt.Sprintf("%03d", i)
 		color := lipgloss.Color(strconv.Itoa(i))
-
 		fmt.Print(
 			style.Background(color).Render(str),
 			style.Foreground(color).Render(str),
 		)
+
+		next := i + 1
+		switch {
+		case next <= 16:
+			switch {
+			case next == 16:
+				fmt.Print("\n\n")
+			case next == 8:
+				fmt.Println()
+			}
+		default:
+			switch {
+			case (next-16)%36 == 0:
+				fmt.Print("\n\n")
+			case (next-16)%6 == 0:
+				fmt.Println()
+			}
+		}
 	}
-	fmt.Println()
 }
