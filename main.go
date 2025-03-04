4926a1bd65c3337ec45969941752e7d6d7cb6458
Gabe Cook <gabe565@gmail.com>
2025-03-04 00:13:28 -0600

    perf: Buffer output

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/muesli/termenv"
@@ -13,29 +16,31 @@ func main() {
 
 	style := lipgloss.NewStyle().Padding(0, 1).Foreground(lipgloss.Color("#fff"))
 
-	for i := range 256 {
+	var buf strings.Builder
+	buf.Grow(20_000)
+	for i := range int64(256) {
 		str := fmt.Sprintf("%03d", i)
-		color := lipgloss.Color(strconv.Itoa(i))
-		fmt.Print(
-			style.Background(color).Render(str),
-			style.Foreground(color).Render(str),
-		)
+		color := lipgloss.Color(strconv.FormatInt(i, 10))
+		buf.WriteString(style.Background(color).Render(str))
+		buf.WriteString(style.Foreground(color).Render(str))
 
 		switch {
 		case i <= 15:
 			switch {
 			case i == 15:
-				fmt.Print("\n\n")
+				buf.WriteString("\n\n")
 			case i == 7:
-				fmt.Println()
+				buf.WriteByte('\n')
 			}
 		default:
 			switch {
 			case (i-15)%36 == 0:
-				fmt.Print("\n\n")
+				buf.WriteString("\n\n")
 			case (i-15)%6 == 0:
-				fmt.Println()
+				buf.WriteByte('\n')
 			}
 		}
 	}
+
+	_, _ = io.WriteString(os.Stdout, buf.String())
 }
